610f7aad78b80c14ce80dcac0074e6b55c4125e4
Fabio Lazari Costa <lazari.developer@outlook.com>
2024-07-11 21:07:41 -0300

    seção 5: Angular: interpolação, diretivas e binding.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+  public widthImg: number = 120;
+  public marginImg: number = 2;
+  public showImg: boolean = true;
+
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredEvents = this.listFilter ? this.filterEvents(this.listFilter) : this.events;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +31,22 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public showImage(){
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
+      response => this.events = this.filteredEvents = response,
       error => console.log(error)
     );
+  }
+
+  public filterEvents(filter: string): any {
+    filter = filter.toLocaleLowerCase();
+    return this.events.filter(
+      (event: any) => event.tema.toLocaleLowerCase().indexOf(filter) !== -1 ||
+                      event.local.toLocaleLowerCase().indexOf(filter) !== -1
+    )
   }
 }
